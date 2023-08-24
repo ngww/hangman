a520cbd5404532b7b0cee6618e749d3af00eedcb
Sithembiso Ngwenya <ngww95@gmail.com>
2023-08-24 22:51:49 +0100

    first commit

---
+++ scripts/word-list.js
@@ -0,0 +1,62 @@
+const wordList = [
+    {
+        word: "dinosaur",
+        hint: "Extinct reptiles that roamed the Earth millions of years ago."
+    },
+    {
+        word: "ocean",
+        hint: "A vast body of saltwater covering much of the Earth's surface."
+    },
+    {
+        word: "guitar",
+        hint: "A musical instrument with strings that you strum or pluck."
+    },
+    {
+        word: "bicycle",
+        hint: "A two-wheeled vehicle that you pedal to move."
+    },
+    {
+        word: "mountain",
+        hint: "A large landform that rises prominently above its surroundings."
+    },
+    {
+        word: "chocolate",
+        hint: "A sweet treat made from cocoa beans."
+    },
+    {
+        word: "umbrella",
+        hint: "A device used to shield from rain or sun."
+    },
+    {
+        word: "dragon",
+        hint: "Mythical creatures often depicted as fire-breathing reptiles."
+    },
+    {
+        word: "bookshelf",
+        hint: "Furniture used for storing books."
+    },
+    {
+        word: "sunglasses",
+        hint: "Eyewear designed to protect your eyes from the sun."
+    },
+    {
+        word: "vacation",
+        hint: "A period of time when you relax and travel for leisure."
+    },
+    {
+        word: "butterfly",
+        hint: "Colorful insects with delicate, winged bodies."
+    },
+    {
+        word: "waterfall",
+        hint: "A natural cascade of water down a steep rock face."
+    },
+    {
+        word: "telescope",
+        hint: "An optical instrument used to observe distant objects in space."
+    },
+    {
+        word: "fireworks",
+        hint: "Hint: Explosive pyrotechnic displays often seen on special occasions."
+    }
+]
